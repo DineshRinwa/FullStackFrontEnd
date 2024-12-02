a37c6b30f07f79a0775c320dedefc4bd1b5d0c19
DineshRinwa <dineshrinwa410@gmail.com>
2024-12-03 00:19:11 +0530

    I done build1

--- FullStackFrontEnd/src/routes/AppRoutes.jsx
+++ FullStackFrontEnd/src/routes/AppRoutes.jsx
@@ -7,10 +7,10 @@ import { Login } from "../pages/Login";
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/product" element={<Product />} />
       <Route path="/create_product" element={<CreateProduct />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
