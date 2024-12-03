3af59086b63032460bfbe4b260223dbb858badb0
DineshRinwa <dineshrinwa410@gmail.com>
2024-12-03 13:15:04 +0530

    I done11

--- FullStackFrontEnd/src/pages/Product.jsx
+++ FullStackFrontEnd/src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { jwtDecode } from "jwt-decode"; // Corrected import
+import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
